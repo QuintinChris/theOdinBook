7cfa918cec063df062f3622bf0898a90e180f26b
QuintinChris <cwq23drexel@gmail.com>
2020-06-03 13:53:55 -0400

    .

--- app.js
+++ app.js
@@ -69,13 +69,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
